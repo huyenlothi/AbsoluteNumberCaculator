ac41dbfcb4c37f3a04840557a999c28f28bb1a14
huyenlothi <69282948+huyenlothi@users.noreply.github.com>
2020-09-21 09:12:28 +0700

    add file

---
+++ AbsoluteNumberCaculator.java
@@ -0,0 +1,8 @@
+public class AbsoluteNumberCaculator {
+    public static int findAbsolute(int number){
+       if (number<0)
+           return -number;
+       else
+           return number;
+    }
+}
